766f8dc18a6493497dc479752870b3f09b3a1efb
ingkejin <ingkejin@gmail.com>
2024-06-10 12:32:45 +0900

    ✨ checkbox 최대 5개 제한, 모든 조건 충족시 submit

--- 태그연습/input/script.js
+++ 태그연습/input/script.js
@@ -1,3 +1,6 @@
+// 복합대입연산자
+let idVeri = pwVeri = pwChkVeri = false;
+
 let userid = document.getElementById('userid');
 let idWarn = document.querySelector('.id-warn');
 
@@ -11,9 +14,12 @@ userid.addEventListener('focusout', function() {
     } else {
     // 8글자 이상일 때 '멋진 아이디네요!' (color : green)
     idWarn.innerHTML = `<span class="txt-green">멋진 아이디네요!</span>`;
+    idVeri = true;
   }
 })
 
+
+// 비밀번호
 let userpw = document.getElementById('userpw');
 let pwWarn = document.querySelector('.pw-warn');
 
@@ -32,15 +38,29 @@ userpw.addEventListener('focusout', function() {
     pwWarn.innerHTML = `<span class="txt-red">8~16자 영문 대 소문자, 숫자를 사용하세요.</span>`
   } else {
     pwWarn.innerHTML = '';
+    pwVeri = true;
   }
 })
 
+
 // 비밀번호 재확인
-// userpw.value 값과 userPwChk.value 값 비교
-// 일치하지 않을 때 => 비밀번호가 일치하지 않습니다. 
-// 일치할 때 => '';
+let userPwChk = document.getElementById('user-pwchk');
+let pwChkWarn = document.querySelector('.pwchk-warn');
+
+userPwChk.addEventListener('focusout', function() {
+  // userpw.value 값과 userPwChk.value 값 비교
+  if(userpw.value == userPwChk.value) {
+    // 일치할 때 => '';
+    pwChkWarn.innerHTML = '';
+    pwChkVeri = true;
+    } else {
+    // 일치하지 않을 때 => 비밀번호가 일치하지 않습니다. 
+    pwChkWarn.innerHTML = `<span class="txt-red">비밀번호가 일치하지 않습니다.</span>`;
+  }
+})
 
 
+// 생년월일
 let year = document.getElementById('year');
 let month = document.getElementById('month');
 let date = document.getElementById('date');
@@ -66,3 +86,33 @@ for(let i = 1; i <= 31; i++) {
   date.insertAdjacentHTML('beforeend', option);
 }
 
+
+// 취미(checkbox) 
+// 5개 까지만 체크 되도록
+let checkBoxs = document.querySelectorAll('input[type="checkbox"][name="hobby"]');
+let maxCount = 5;
+
+checkBoxs.forEach((item) => {
+  item.addEventListener('change', () => {
+    let checkCount = document.querySelectorAll('input[type="checkbox"][name="hobby"]:checked').length;
+    
+    // alert('최대 5개까지만 선택할 수 있습니다.')
+    if(checkCount > maxCount) {
+      item.checked = false; // 6번째 체크박스 체크 해제
+      alert('최대 5개까지만 선택할 수 있습니다.');
+    }
+  })
+})
+
+
+let submitBtn = document.getElementById('submit-btn');
+let joinForm = document.getElementById('join-form');
+
+submitBtn.addEventListener('click', (e) => {
+  if(idVeri && pwVeri && pwChkVeri) {
+    joinForm.submit();
+  } else {
+    // 기본 속성 제거
+    e.preventDefault();
+  }
+})

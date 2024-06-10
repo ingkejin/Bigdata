3e36d9ea427942792dad40b9dde9043a83103b40
ingkejin <ingkejin@gmail.com>
2024-06-10 11:08:47 +0900

    ✨ 아이디 8글자 이상 조건문

--- 태그연습/input/script.js
+++ 태그연습/input/script.js
@@ -1,9 +1,26 @@
+let userid = document.getElementById('userid');
+let idWarn = document.querySelector('.id-warn');
+
+// keyup : 키를 눌렀다가 놨을 때 발생
+userid.addEventListener('focusout', function() {
+  // userid.value => input에 입력된 텍스트
+  // userid.value.length => input 텍스트 길이
+  if(userid.value.length < 8) {
+    // 8글자 미만일 때 '아이디를 8글자 이상 입력하세요.' (color : red)
+    idWarn.innerHTML = `<span class="txt-red">아이디를 8글자 이상 입력하세요.</span>`;
+    } else {
+    // 8글자 이상일 때 '멋진 아이디네요!' (color : green)
+    idWarn.innerHTML = `<span class="txt-green">멋진 아이디네요!</span>`;
+  }
+})
+
 let year = document.getElementById('year');
 let month = document.getElementById('month');
 let date = document.getElementById('date');
 
 let kst = new Date();
 let getYear = kst.getFullYear();
+
 
 // 1920 i값이 getYear(2024)와 작거나 같을 때 까지
 // i를 1씩 더하며 반복
